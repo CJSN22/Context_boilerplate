6f0c5b71ed06a4d3556541925e652866a4b0643c
Charles Fabricius --- CJ <cjsn22@gmail.com>
2019-07-08 03:05:45 +0000

    first commit

---
+++ src/js/store/flux.js
@@ -0,0 +1,67 @@
+const getState = ({ getStore, setStore }) => {
+	return {
+		store: {
+			demo: [
+				{
+					title: "FIRST",
+					background: "white",
+					initial: "white"
+				},
+				{
+					title: "SECOND",
+					background: "white",
+					initial: "white"
+				},
+				{
+					title: "THIRD",
+					background: "white",
+					initial: "white"
+				},
+				{
+					title: "FOURTH",
+					background: "white",
+					initial: "white"
+				},
+				{
+					title: "FIVE",
+					background: "white",
+					initial: "white"
+				}
+			]
+		},
+
+		actions: {
+			changeColor: (index, color) => {
+				//get the store
+				const store = getStore();
+
+				//we have to loop the entire demo array to look for the respective index
+				//and change its color
+				const demo = store.demo.map((elm, i) => {
+					if (i === index) elm.background = color;
+					return elm;
+				});
+
+				//reset the global store
+				setStore({ demo: demo });
+			},
+
+			resetColor: () => {
+				//get the store
+				const store = getStore();
+
+				//we have to loop the entire demo array to look for the respective index
+				//and change its color
+				const demo = store.demo.map((elm, i) => {
+					elm.background = "white";
+					return elm;
+				});
+
+				//reset the global store
+				setStore({ demo: demo });
+			}
+		}
+	};
+};
+
+export default getState;
